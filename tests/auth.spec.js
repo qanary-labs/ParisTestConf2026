8a8e973eaac9a9a0fa0b3eb07ce6fac1badcccf8
Steven PESCHETEAU <steven.pescheteau@gmail.com>
2026-02-24 17:50:42 +0100

    Initial commit

---
+++ tests/auth.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Auth flow", () => {
+  
+  test("J'accède à la page d'administration en me connectant, puis je me déconnecte", async ({ page }) => {
+    await page.goto("/");
+
+    await page.getByLabel("Username").fill("admin");
+    await page.getByLabel("Password").fill("password");
+    await page.getByRole("button", { name: /se connecter/i }).click();
+
+    await expect(page).toHaveURL(/\/admin$/);
+    await expect(page.getByRole("heading", { name: /zone admin protégée/i })).toBeVisible();
+    await expect(page.getByText(/connecté en tant que admin/i)).toBeVisible();
+
+    await page.getByRole("button", { name: /se déconnecter/i }).click();
+
+    await expect(page).toHaveURL(/\/\?logged_out=1$/);
+    await expect(page.getByText(/vous avez été déconnecté/i)).toBeVisible();
+  });
+});
